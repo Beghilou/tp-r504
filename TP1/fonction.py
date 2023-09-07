2a49838ca1f58e20f16c56fbcee1c786ed7f7f2b
Beghilou <137314345+Beghilou@users.noreply.github.com>
2023-09-07 16:38:52 +0200

    Update fonction.py

--- TP1/fonction.py
+++ TP1/fonction.py
@@ -2,8 +2,8 @@ def Puissance_2(a,b):
     if not (isinstance(A, (int, float)) and isinstance(B, int)):
         raise ValueError("A doit être un nombre (int ou float) et B doit être un nombre entier.")
 
-    if B < 0:
-        raise ValueError("B doit être un nombre entier non négatif.")
+    if B < 0 or (A == 0 and B == 0):
+        raise ValueError("B doit être un nombre entier non négatif, et A ne peut pas être zéro si B est zéro.")
 
     return A ** B
 
@@ -22,6 +22,12 @@ except ValueError as e:
 try:
     resultat = puissance(2, -3)
     print(f"2 à la puissance -3 est égal à : {resultat}")
+except ValueError as e:
+    print(f"Erreur: {e}")
+
+try:
+    resultat = puissance(0, 0)
+    print(f"0 à la puissance 0 est égal à : {resultat}")
 except ValueError as e:
     print(f"Erreur: {e}")
 
