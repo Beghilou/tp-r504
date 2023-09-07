5eef2632fb960945e08eb8ab6d6102cf65847b7c
Beghilou <137314345+Beghilou@users.noreply.github.com>
2023-09-07 16:37:07 +0200

    Update fonction.py

--- TP1/fonction.py
+++ TP1/fonction.py
@@ -1,10 +1,29 @@
 def Puissance_2(a,b):
-	try:
-		pow = 1
-		for x in range (b):
-			pow = pow*a
-		return pow
-	except TypeError:
-		raise TypeError("Only integers are allowed")
+    if not (isinstance(A, (int, float)) and isinstance(B, int)):
+        raise ValueError("A doit être un nombre (int ou float) et B doit être un nombre entier.")
+
+    if B < 0:
+        raise ValueError("B doit être un nombre entier non négatif.")
+
+    return A ** B
+
+try:
+    resultat = puissance("2", 3)
+    print(f"2 à la puissance 3 est égal à : {resultat}")
+except ValueError as e:
+    print(f"Erreur: {e}")
+
+try:
+    resultat = puissance(2, 2.5)
+    print(f"2 à la puissance 2.5 est égal à : {resultat}")
+except ValueError as e:
+    print(f"Erreur: {e}")
+
+try:
+    resultat = puissance(2, -3)
+    print(f"2 à la puissance -3 est égal à : {resultat}")
+except ValueError as e:
+    print(f"Erreur: {e}")
+
 		
 	
