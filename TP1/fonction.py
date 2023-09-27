5225dcdab6b9089189b52aac58e4d91e0430767f
louis <louis.beghin29@gmail.com>
2023-09-27 16:28:10 +0200

    fonction 3

--- TP1/fonction.py
+++ TP1/fonction.py
@@ -1,35 +1,18 @@
-def Puissance_2(a,b):
-    if not (isinstance(A, (int, float)) and isinstance(B, int)):
-        raise ValueError("A doit être un nombre (int ou float) et B doit être un nombre entier.")
+import pytest
 
-    if B < 0 or (A == 0 and B == 0):
-        raise ValueError("B doit être un nombre entier non négatif, et A ne peut pas être zéro si B est zéro.")
-
-    return A ** B
-
-try:
-    resultat = Puissance_2(2, 3)
-    print(f"2 à la puissance 3 est égal à : {resultat}")
-except ValueError as e:
-    print(f"Erreur: {e}")
-
-try:
-    resultat = Puissance_2(2, 2.5)
-    print(f"2 à la puissance 2.5 est égal à : {resultat}")
-except ValueError as e:
-    print(f"Erreur: {e}")
-
-try:
-    resultat = Puissance_2(2, -3)
-    print(f"2 à la puissance -3 est égal à : {resultat}")
-except ValueError as e:
-    print(f"Erreur: {e}")
-
-try:
-    resultat = Puissance_2(0, 0)
-    print(f"0 à la puissance 0 est égal à : {resultat}")
-except ValueError as e:
-    print(f"Erreur: {e}")
+def Puissance_2(a, b):
+    a, b = int(a), int(b)
+    if not isinstance(b, int):
+        raise ValueError("La valeur de l'exposant doit être un nombre entier.")
+    if b == 0:
+        raise ZeroDivisionError("L'exposant ne doit pas être 0.")
+    if b <= 0:
+        raise ValueError("L'exposant doit être un nombre entier positif.")
+    try:
+        result = int(a) ** int(b)
+        return result
+    except ValueError:
+        raise ValueError("Les valeurs de la base et de l'exposant doivent être des nombres entiers.")
 
 		
 	
